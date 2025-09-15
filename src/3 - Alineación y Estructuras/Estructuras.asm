03fecb33acbd21e6023ac8efb2f9ae44aaddb434
luciima <luciiamar19@gmail.com>
2025-09-15 17:13:13 -0300

    Agrega ejercicio 2 de Estructuras OK

--- src/3 - Alineación y Estructuras/Estructuras.asm
+++ src/3 - Alineación y Estructuras/Estructuras.asm
@@ -2,19 +2,19 @@
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
-NODO_OFFSET_NEXT EQU 8
+NODO_OFFSET_NEXT EQU 0
 NODO_OFFSET_CATEGORIA EQU 8
-NODO_OFFSET_ARREGLO EQU 8
-NODO_OFFSET_LONGITUD EQU 8
-NODO_SIZE EQU 8
-PACKED_NODO_OFFSET_NEXT EQU 8
+NODO_OFFSET_ARREGLO EQU 16
+NODO_OFFSET_LONGITUD EQU 24
+NODO_SIZE EQU 32
+PACKED_NODO_OFFSET_NEXT EQU 0
 PACKED_NODO_OFFSET_CATEGORIA EQU 8
-PACKED_NODO_OFFSET_ARREGLO EQU 8
-PACKED_NODO_OFFSET_LONGITUD EQU 8
-PACKED_NODO_SIZE EQU 8
-LISTA_OFFSET_HEAD EQU 8
+PACKED_NODO_OFFSET_ARREGLO EQU 9
+PACKED_NODO_OFFSET_LONGITUD EQU 17
+PACKED_NODO_SIZE EQU 21
+LISTA_OFFSET_HEAD EQU 0
 LISTA_SIZE EQU 8
-PACKED_LISTA_OFFSET_HEAD EQU 8
+PACKED_LISTA_OFFSET_HEAD EQU 0
 PACKED_LISTA_SIZE EQU 8
 
 ;########### SECCION DE DATOS
@@ -29,12 +29,45 @@ global cantidad_total_de_elementos_packed
 
 ;########### DEFINICION DE FUNCIONES
 ;extern uint32_t cantidad_total_de_elementos(lista_t* lista);
-;registros: lista[?]
+;registros: lista[RDI]
 cantidad_total_de_elementos:
+	push RBP
+	mov RBP, RSP
+
+	mov R12D, 0 ; contador
+	mov R13, [RDI]
+
+	.ciclo:
+	cmp R13, 0 
+	je .fin
+	mov EAX, dword [R13+24]
+	mov R13, [R13]
+	add R12D, EAX
+	jmp .ciclo
+	
+	.fin:
+	mov EAX, R12D
+	pop RBP
 	ret
 
 ;extern uint32_t cantidad_total_de_elementos_packed(packed_lista_t* lista);
 ;registros: lista[?]
 cantidad_total_de_elementos_packed:
-	ret
+	push RBP
+	mov RBP, RSP
 
+	mov R12D, 0 ; contador
+	mov R13, [RDI]
+
+	.ciclo:
+	cmp R13, 0 
+	je .fin
+	mov EAX, dword [R13+17]
+	mov R13, [R13]
+	add R12D, EAX
+	jmp .ciclo
+	
+	.fin:
+	mov EAX, R12D
+	pop RBP
+	ret
