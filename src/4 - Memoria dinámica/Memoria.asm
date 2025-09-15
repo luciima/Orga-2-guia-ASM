49a3d9c5a29f651b8ae9ef281875ca50d1c8e477
luciima <luciiamar19@gmail.com>
2025-09-15 18:07:26 -0300

    Agrega parte del ej. 4, strLen y strCmp OK

--- src/4 - Memoria dinámica/Memoria.asm
+++ src/4 - Memoria dinámica/Memoria.asm
@@ -16,6 +16,38 @@ global strLen
 
 ; int32_t strCmp(char* a, char* b)
 strCmp:
+	; RDI char* a, RSI char* b
+	push RBP
+	mov RBP, RSP
+
+	xor RAX, RAX
+	mov R8, 0
+
+	.ciclo:
+	mov DL, byte [RDI+R8]
+	mov CL, byte [RSI+R8]
+	cmp DL, CL
+	ja .mayorA
+	jb .mayorB
+	add R8, 1 
+	cmp DL, 0
+	je .iguales
+	jmp .ciclo
+
+	.mayorA:
+	mov EAX, -1
+	jmp .fin
+
+	.mayorB:
+	mov EAX, 1
+	jmp .fin
+
+	.iguales:
+	mov EAX, 0
+	jmp .fin
+
+	.fin:
+	pop RBP
 	ret
 
 ; char* strClone(char* a)
@@ -32,6 +64,23 @@ strPrint:
 
 ; uint32_t strLen(char* a)
 strLen:
+	; RDI char* a
+	push RBP
+	mov RBP, RSP
+
+	xor RAX, RAX
+	mov R8D, 0 ;length
+
+	.ciclo:
+	mov DL, byte [RDI+R8]
+	cmp DL, 0
+	je .fin
+	add R8D, 1
+	jmp .ciclo
+
+	.fin:
+	mov EAX, R8D
+	pop RBP
 	ret
 
 
