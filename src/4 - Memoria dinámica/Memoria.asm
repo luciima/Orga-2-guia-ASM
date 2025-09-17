a3d2ef6f5836f145ae0ff284ca7e4abd6567cbdb
luciima <luciiamar19@gmail.com>
2025-09-17 14:51:48 -0300

    Agrega parte del ej. 4, strClone OK

--- src/4 - Memoria dinámica/Memoria.asm
+++ src/4 - Memoria dinámica/Memoria.asm
@@ -52,10 +52,56 @@ strCmp:
 
 ; char* strClone(char* a)
 strClone:
+	push RBP
+	mov RBP, RSP
+	push R12
+	push R13
+	push R14
+	push R15
+
+	mov R12, RDI
+
+	call strLen 
+
+	xor RDI, RDI
+	add EAX, 1
+	mov EDI, EAX 
+	mov R14D, EAX
+
+	call malloc
+
+	mov R13, RAX
+	mov R15, RAX
+
+	.loop:
+	cmp R14D, 1
+	je .fin
+	mov AL, byte [R12]
+	mov [R13], AL
+	add R13, 1
+	add R12, 1 
+	sub R14D, 1 
+	jmp .loop
+
+	.fin:
+	mov [R13], byte 0
+	mov RAX, R15
+	pop R15 
+	pop R14
+	pop R13
+	pop R12
+	pop RBP
 	ret
 
 ; void strDelete(char* a)
 strDelete:
+	; RDI char* a
+	push RBP
+	mov RBP, RSP
+
+	call free
+
+	pop RBP
 	ret
 
 ; void strPrint(char* a, FILE* pFile)
